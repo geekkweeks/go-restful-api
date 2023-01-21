1254d8f6af278cefd7dee603bd293564aab77352
geekkweeks <zahgreat30@gmail.com>
2023-01-21 07:24:28 +0700

    add user interface

---
+++ repository/user_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"geekkweeks/go-restful-api/model/domain"
+)
+
+type UserRepository interface {
+	Add(ctx context.Context, tx sql.Tx, user domain.User) domain.User
+	Update(ctx context.Context, tx sql.Tx, user domain.User) domain.User
+	Delete(ctx context.Context, tx sql.Tx, id int)
+	FindById(ctx context.Context, tx sql.Tx, id int) domain.User
+	GetAll(ctx context.Context, tx sql.Tx) []domain.User
+}
